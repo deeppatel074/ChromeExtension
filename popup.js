f60179128cc9bd580396ba468509005c6dc46920
Battel <46970980+Battel@users.noreply.github.com>
2019-10-18 19:17:33 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@ chrome.tabs.query({
     active: true,
     lastFocusedWindow: true
 }, function(tabs) {
-    // and use that tab to fill in out title and url
     var tab = tabs[0];
     console.log(tab.url);
     document.write(tab.url);
